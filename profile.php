f3d8c58c467c01b33d401238a57f7270b3ea1a3b
visocquezaya2-cmyk <visoc.quezaya2@gmail.com>
2026-02-01 12:10:56 +0800

    Initial commit - profile page

---
+++ profile.php
@@ -0,0 +1,25 @@
+<?php
+$project = "Student Information";
+$name = "Name: Quezaya Visoc"; 
+$Address = "Address: Dapdap, Catarman, Liloan, Cebu"; 
+$email = "Email: quezayavisoc9@gmail.com"; 
+$Year = "Year: 2nd Year";
+$program = "Program: BSIT";
+$Section = "Section: IT22C"; 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profile</title>
+</head>
+<body>
+    <h1><?php echo $project; ?></h1>
+     <p><?php echo $name; ?></p>
+    <p><?php echo $Address; ?></p>
+     <p><?php echo $email; ?></p>
+      <p><?php echo $Year; ?></p>
+       <p><?php echo $program; ?></p>
+        <p><?php echo $Section; ?></p>
+</body>
+</html>
